53792edeb18c29601cafd0f6b1a00452ac9db4a3
Audry Ford <audryford792@gmail.com>
2021-07-03 22:20:53 -0500

    add html element with user input with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,25 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+   
+    // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // Create div to dols task infor and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
